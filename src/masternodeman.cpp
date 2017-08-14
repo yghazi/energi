77139ecdd6b8eee47f4cd93aae1afa3ffae62611
yghazi <yumnaghazi@gmail.com>
2017-08-15 00:55:47 +0500

    Rebrand src files after dash commits

--- src/masternodeman.cpp
+++ src/masternodeman.cpp
@@ -803,7 +803,7 @@ std::pair<CService, std::set<uint256> > CMasternodeMan::PopScheduledMnbRequestCo
 
 void CMasternodeMan::ProcessMessage(CNode* pfrom, std::string& strCommand, CDataStream& vRecv)
 {
-    if(fLiteMode) return; // disable all Dash specific functionality
+    if(fLiteMode) return; // disable all Energi specific functionality
     if(!masternodeSync.IsBlockchainSynced()) return;
 
     if (strCommand == NetMsgType::MNANNOUNCE) { //Masternode Broadcast
