fb3074ed6a9f195a05526ad0b02b9f2a4f2adf7d
Ryan Lucchese <ryan@energi.team>
2017-11-21 21:00:57 -0700

    fixing grammatical error

--- src/qt/askpassphrasedialog.cpp
+++ src/qt/askpassphrasedialog.cpp
@@ -124,7 +124,7 @@ void AskPassphraseDialog::accept()
                                          "<qt>" +
                                          tr("Energi Core will close now to finish the encryption process. "
                                          "Remember that encrypting your wallet cannot fully protect "
-                                         "your energis from being stolen by malware infecting your computer.") +
+                                         "your EGI from being stolen by malware infecting your computer.") +
                                          "<br><br><b>" +
                                          tr("IMPORTANT: Any previous backups you have made of your wallet file "
                                          "should be replaced with the newly generated, encrypted wallet file. "
