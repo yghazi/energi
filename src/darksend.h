18e75685ffe46890a14d217aaf9a3e6fb59a050d
Ryan Lucchese <ryan@energi.team>
2017-11-21 21:43:23 -0700

    restoring dash files that had been renamed to energi

--- src/darksend.h
+++ src/darksend.h
@@ -2,6 +2,9 @@
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+#include <assert>
+static_assert<false, "TODO: is this still compiled?">
+
 #ifndef DARKSEND_H
 #define DARKSEND_H
 
