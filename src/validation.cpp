61fa8258cd53e27bc6ddd6120020f94393e1d1c0
Ryan Lucchese <ryan@energi.team>
2017-11-30 21:21:48 -0700

    removed difficulty check for DGW pre fork

--- src/validation.cpp
+++ src/validation.cpp
@@ -3327,28 +3327,19 @@ static bool CheckIndexAgainstCheckpoint(const CBlockIndex* pindexPrev, CValidati
 bool ContextualCheckBlockHeader(const CBlockHeader& block, CValidationState& state, CBlockIndex * const pindexPrev)
 {
     const Consensus::Params& consensusParams = Params().GetConsensus();
-    uint32_t const nHeight = static_cast<uint32_t>(pindexPrev->nHeight) + 1; // TODO: Why is CBlockIndex::nHeight signed?
+    uint32_t const nHeight = static_cast<uint32_t>(pindexPrev->nHeight) + 1;
 
     // height check
     if (block.nHeight != nHeight)
     {
-        return state.DoS(100, error("%s: block height is invalid", __func__), REJECT_INVALID, "bad-blk-height");
+        return state.DoS(100, error("%s: block height is invalid", __func__),
+                            REJECT_INVALID, "bad-blk-height");
     }
 
     // Check proof of work
-    // TODO: disable pre-DGW fork logic
-    if(Params().NetworkIDString() == CBaseChainParams::MAIN && nHeight <= 68589){
-        // architecture issues with DGW v1 and v2)
-        unsigned int nBitsNext = GetNextWorkRequired(pindexPrev, &block, consensusParams);
-        double n1 = ConvertBitsToDouble(block.nBits);
-        double n2 = ConvertBitsToDouble(nBitsNext);
-
-        if (abs(n1-n2) > n1*0.5)
-            return state.DoS(100, error("%s : incorrect proof of work (DGW pre-fork) - %f %f %f at %d", __func__, abs(n1-n2), n1, n2, nHeight),
-                            REJECT_INVALID, "bad-diffbits");
-    } else {
-        if (block.nBits != GetNextWorkRequired(pindexPrev, &block, consensusParams))
-            return state.DoS(100, error("%s : incorrect proof of work at %d", __func__, nHeight),
+    if (block.nBits != GetNextWorkRequired(pindexPrev, &block, consensusParams))
+    {
+        return state.DoS(100, error("%s : incorrect proof of work at %d", __func__, nHeight),
                             REJECT_INVALID, "bad-diffbits");
     }
 
