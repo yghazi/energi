59e57a013d22f104d0111f35633b5a0a74d6da89
Ryan Lucchese <ryan@energi.team>
2017-11-30 21:23:12 -0700

    removed TODO

--- src/validation.cpp
+++ src/validation.cpp
@@ -3349,7 +3349,6 @@ bool ContextualCheckBlockHeader(const CBlockHeader& block, CValidationState& sta
                              REJECT_INVALID, "time-too-old");
 
     // Reject block.nVersion=1 blocks when 95% (75% on testnet) of the network has upgraded:
-    // TODO: make sure Energi starts at the latest block version
     if (block.nVersion < 2 && IsSuperMajority(2, pindexPrev, consensusParams.nMajorityRejectBlockOutdated, consensusParams))
         return state.Invalid(error("%s: rejected nVersion=1 block", __func__),
                              REJECT_OBSOLETE, "bad-version");
